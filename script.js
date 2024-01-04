c0e437196a51f8f905f21a6697c03c813079f2fb
Ali Hussain <ahalihussain11@gmail.com>
2024-01-04 21:47:47 +0500

    Todo list completed

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// const container = document.querySelector(".container"),
-    //   pwFields = document.querySelectorAll(".password"),
-    //   login = document.querySelector(".login-link");
+// for login-page
 function validateForm() {
     var email = document.getElementById('email').value.trim();
     var password = document.getElementById('password').value.trim();
@@ -9,10 +7,53 @@ function validateForm() {
       alert('Please enter both email and password.');
       return false;
     }
-
     
-
+   
     return true;
   }
+
+  // for todolist
+  const inputBox = document.getElementById("input-box");
+  const listContainer = document.getElementById("list-container");
+
+
+  function addTask() {
+   if (inputBox.value === ''){
+    alert("You must write something");
+   }
+
+   else{
+    let li = document.createElement("li")
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+   }
+   inputBox.value = "";
+   saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+   else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+  }
+
+ }, false);  
+
+ function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+ }
+
+ function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+ }
+ showTask();
     
 
