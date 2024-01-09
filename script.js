6f5c8d0ed2fefa1e93d2883dcdf13299163fe93c
syedMSohaib <syedmsohaib68@gmail.com>
2024-01-10 00:27:36 +0500

    update html and ui

--- script.js
+++ script.js
@@ -1,73 +1,104 @@
 // for login-page
-function validateForm() {
-    var email = document.getElementById('email').value.trim();
-    var password = document.getElementById('password').value.trim();
 
-    if (email === '' || password === '') {
-      alert('Please enter both email and password.');
-      return false;
-    }
-    
-   
-    return true;
+function submitLogin(e) {
+  e.preventDefault()
+
+  console.log('here')
+
+  const signupForm = document.getElementById('signupForm')
+  let formData = new FormData(signupForm)
+  const entries = Object.fromEntries(formData)
+
+  if (
+    entries.email === 'mytodolist@gmail.com' &&
+    entries.password === 'test123'
+  ) {
+    alert('Login successful')
+    window.location.href = '/main.html'
+  } else {
+    alert('Invalid Login credentials')
   }
-
-  function validateEmail(email){
-    // Regular expressiom for simple email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-
-  const email = "mytodolist@email.com";
-  if (validateEmail(email)){
-    console.log("Email is valid");
-  }
-  else {
-    console.log("Email is invalid");
-  }
-
-  // for todolist
-  const inputBox = document.getElementById("input-box");
-  const listContainer = document.getElementById("list-container");
-
-
-  function addTask() {
-   if (inputBox.value === ''){
-    alert("You must write something");
-   }
-
-   else{
-    let li = document.createElement("li")
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-   }
-   inputBox.value = "";
-   saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData();
+function submitTodo(e) {
+  e.preventDefault()
+  const todoForm = document.getElementById('todoForm')
+  let formData = new FormData(todoForm)
+  const entries = Object.fromEntries(formData)
+
+  if (entries.task_name === '' || entries.description === '') {
+    alert('Please enter task name and description')
+
+    return
   }
-   else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove();
-    saveData();
-  }
 
- }, false);  
+  console.log(entries)
+}
 
- function saveData(){
-  localStorage.setItem("data", listContainer.innerHTML);
- }
+// function validateForm() {
+//   var email = document.getElementById('email').value.trim()
+//   var password = document.getElementById('password').value.trim()
 
- function showTask(){
-  listContainer.innerHTML = localStorage.getItem("data");
- }
- showTask();
-    
+//   if (email === '' || password === '') {
+//     alert('Please enter both email and password.')
+//     return false
+//   }
 
+//   return true
+// }
+
+// function validateEmail(email) {
+//   // Regular expressiom for simple email validation
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+//   return emailRegex.test(email)
+// }
+
+// const email = 'mytodolist@email.com'
+// if (validateEmail(email)) {
+//   console.log('Email is valid')
+// } else {
+//   console.log('Email is invalid')
+// }
+
+// // for todolist
+// const inputBox = document.getElementById('input-box')
+// const listContainer = document.getElementById('list-container')
+
+// function addTask() {
+//   if (inputBox.value === '') {
+//     alert('You must write something')
+//   } else {
+//     let li = document.createElement('li')
+//     li.innerHTML = inputBox.value
+//     listContainer.appendChild(li)
+
+//     let span = document.createElement('span')
+//     span.innerHTML = '\u00d7'
+//     li.appendChild(span)
+//   }
+//   inputBox.value = ''
+//   saveData()
+// }
+
+// listContainer.addEventListener(
+//   'click',
+//   function (e) {
+//     if (e.target.tagName === 'LI') {
+//       e.target.classList.toggle('checked')
+//       saveData()
+//     } else if (e.target.tagName === 'SPAN') {
+//       e.target.parentElement.remove()
+//       saveData()
+//     }
+//   },
+//   false,
+// )
+
+// function saveData() {
+//   localStorage.setItem('data', listContainer.innerHTML)
+// }
+
+// function showTask() {
+//   listContainer.innerHTML = localStorage.getItem('data')
+// }
+// showTask()
